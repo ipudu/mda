75084cbe39c5b42161d2c20a3b18a7ff77934a76
Pu Du <rocketsboy@gmail.com>
2019-06-28 16:30:22 -0500

    change the logo art

--- mda/util.py
+++ mda/util.py
@@ -30,18 +30,19 @@ def output_welcome():
     
     #............................................................................
     logo = """
-                                            88            
-                                            88            
-                                            88            
-                88,dPYba,,adPYba,    ,adPPYb,88  ,adPPYYba,
-                88P'   "88"    "8a  a8"    `Y88  ""     `Y8
-                88      88      88  8b       88  ,adPPPPP88
-                88      88      88  "8a,   ,d88  88,    ,88
-                88      88      88   `"8bbdP"Y8  `"8bbdP"Y8
-                
-                   mda: analysis tools for MD simulations
+                                                                    
+                88888888ba,    88b           d88         db         
+                88      `"8b   888b         d888        d88b        
+                88        `8b  88`8b       d8'88       d8'`8b       
+                88         88  88 `8b     d8' 88      d8'  `8b      
+                88         88  88  `8b   d8'  88     d8YaaaaY8b     
+                88         8P  88   `8b d8'   88    d8''''''''8b    
+                88      .a8P   88    `888'    88   d8'        `8b   
+                88888888Y"'    88     `8'     88  d8'          `8b  
+                                                                
+                     MDA: analysis tools for MD simulations
             """
-    
+
     logo += '\n' + '.' * width
     print(logo)
 
